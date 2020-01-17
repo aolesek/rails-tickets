186ef44e46f8450939628e6665d14639a4728e0b
aolesek <arkadiusz.olesek@gmail.com>
2020-01-17 08:06:54 +0100

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :events, :only => [:index, :new, :create, :show]
   # get 'events/show'
   get 'events/:id' => 'events#show'
   resources :tickets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root :to => "tickets#index"
+  root :to => "events#index"
 end
